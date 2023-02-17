fb500d6174e8a3224bbb66621c68965277d5c2b0
Jessy Andujar Cruz <124212160+J3SSY-ANDU@users.noreply.github.com>
2023-02-16 23:06:19 -0400

    JSON, DOM, Events

---
+++ JavaScript/Week 3/JavaScript in the Browser/webPageContentUpdate.js
@@ -0,0 +1,24 @@
+// To capture user input, you can use the built-in prompt() method, like this:
+let answer = prompt('What is your name?');
+if (typeof(answer) === 'string') {
+    var h1 = document.createElement('h1')
+    h1.innerText = answer;
+    document.body.innerText = '';
+    document.body.appendChild(h1);
+}
+
+
+// This is probably the quickest and easiest way to capture user input on a website, but doing it this way is not the most efficient approach, especially in more complex scenarios.
+var h1 = document.createElement('h1')
+h1.innerText = "Type into the input to make this text change"
+
+var input = document.createElement('input')
+input.setAttribute('type', 'text')
+
+document.body.innerText = '';
+document.body.appendChild(h1);
+document.body.appendChild(input);
+
+input.addEventListener('change', function() {
+    h1.innerText = input.value
+})
